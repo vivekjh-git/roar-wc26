25501f20e7e8eb55585df7a331328606ddd59603
vivekjh-git <vivekjung.djinworks@gmail.com>
2026-06-28 12:22:07 +0545

    fix turbopack build error with next-pwa by forcing webpack

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const withPWA = require("next-pwa")({
 });
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "flagcdn.com" },
